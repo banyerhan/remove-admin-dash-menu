71a1ddebcec439d3bc1716d4ac6d0eb0d00c602e
Night Fury <38713400+banyerhan@users.noreply.github.com>
2018-09-27 22:06:06 +0630

    Create remove_admin_menu.php

---
+++ remove_admin_menu.php
@@ -0,0 +1,15 @@
+<?php
+function remove_menus () {
+    if(is_user_logged_in() && current_user_can('author'))
+    {
+        global $menu;
+        $restricted = array(__('Downloads'),__('Grid Elements'),__('Contact'), __('Links'), __('Pages'), __('Appearance'), __('Tools'), __('Settings'), __('Comments'), __('Plugins'));
+        end ($menu);
+        while (prev($menu)){
+            $value = explode(' ',$menu[key($menu)][0]);
+            if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+        }
+    }
+}
+add_action('admin_menu', 'remove_menus');
+?>
