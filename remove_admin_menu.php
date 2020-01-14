8b171b39da4e4d7929d1ccaa102c8e88acf60fdd
Night Fury <banyerhan@gmail.com>
2020-01-14 21:00:50 +0630

    Update remove_admin_menu.php

--- remove_admin_menu.php
+++ remove_admin_menu.php
@@ -54,5 +54,11 @@ add_action( 'wp_before_admin_bar_render', 'remove_new_content' );
 // get current users
 
 global $current_user;
-   
 $current_user =  wp_get_current_user();
+
+printf( __( 'Username: %s', 'textdomain' ), esc_html( $current_user->user_login ) ) . '<br />';
+printf( __( 'User email: %s', 'textdomain' ), esc_html( $current_user->user_email ) ) . '<br />';
+printf( __( 'User first name: %s', 'textdomain' ), esc_html( $current_user->user_firstname ) ) . '<br />';
+printf( __( 'User last name: %s', 'textdomain' ), esc_html( $current_user->user_lastname ) ) . '<br />';
+printf( __( 'User display name: %s', 'textdomain' ), esc_html( $current_user->display_name ) ) . '<br />';
+printf( __( 'User ID: %s', 'textdomain' ), esc_html( $current_user->ID ) );
